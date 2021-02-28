e6689c861cab95bab28be28a5e9ac50e2e63efe5
fill shmykov <fshmykov@gmail.com>
2021-02-28 19:11:30 +0400

    min max

--- MinMax/Program.cs
+++ MinMax/Program.cs
@@ -6,7 +6,35 @@ namespace MinMax
     {
         static void Main(string[] args)
         {
-            
+            // написать программу, которая найде min и max в массиве
+            // int i = 0;
+            // int[] a = new[] { -1, 2, -3, 10, 7, 3, -5, 8, -10, -3, 2, -7 };
+            int[] a = null;
+
+            if (a != null && a.Length > 0)
+            {
+                // todo
+                int min = a[0];
+                int max = a[0];
+
+                for (var i = 0; i < a.Length; i++)
+                {
+                    if (min > a[i])
+                        min = a[i];
+
+                    if (max < a[i])
+                        max = a[i];
+                }
+
+                Console.WriteLine($"min={min}\nmax={max}");
+                // найти и вывести в консоль
+            }
+            else
+            {
+                Console.WriteLine($"массив пустой");
+            }
+
+            Console.ReadKey();
         }
     }
 }
