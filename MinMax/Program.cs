2a3417b55120e82f6b1763c892a220645a359e1d
fill shmykov <fshmykov@gmail.com>
2021-02-28 19:26:03 +0400

    + while

--- MinMax/Program.cs
+++ MinMax/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace MinMax
@@ -10,6 +11,8 @@ namespace MinMax
             // написать программу, которая найде min и max в массиве
             // int i = 0;
             int[] a = new[] { -1, 2, -3, 10, 7, 3, -5, 8, -10, -3, 2, -7 };
+            var b = new List<int>(a);
+
             // int[] a = null;
 
             if (a != null && a.Length > 0)
@@ -21,13 +24,25 @@ namespace MinMax
                 int min = a[0];
                 int max = a[0];
 
-                for (int i = 0; i < a.Length; i++)
-                {
-                    if (min > a[i])
-                        min = a[i];
+                //for (int i = 0; i < a.Length; i++)
+                //{
+                //    if (min > a[i])
+                //        min = a[i];
 
-                    if (max < a[i])
-                        max = a[i];
+                //    if (max < a[i])
+                //        max = a[i];
+                //}
+
+                
+                while(b.Count > 0)
+                {
+                    if (min > b[0])
+                        min = b[0];
+
+                    if (max < b[0])
+                        max = b[0];
+
+                    b.RemoveAt(0);
                 }
 
                 //foreach(var item in a)
