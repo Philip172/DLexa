81a90bab414a45cfa917d9d308674bc5f2efda89
fill shmykov <fshmykov@gmail.com>
2021-03-01 19:33:53 +0400

    + fibonache, sorting

---
+++ Fibonacci numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fibonacci_numbers
+{
+    class Program
+    {
+        //вычислить 15е число Фиббоначи
+        static void Main(string[] args)
+        {
+            int a = 1, prea=1;
+
+            Console.WriteLine("15е число Фиббоначи:");
+            for(var i = 1; i<=15; i++)
+            {
+                a += prea;
+                prea = a;
+                //Console.WriteLine($"число {i}: {a}");
+            }
+            Console.Write(a);
+            Console.ReadKey();
+        }
+    }
+}
